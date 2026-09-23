f503bb3decb94c7a177854318946f92a679b77f6
KatherineA <kath.avila@duocuc.cl>
2026-09-22 22:12:28 -0300

    -Estructura visual principal de las clases de VentanaPrincipal y de VentanaRegistroPedido. - creación de una clase de tipo enum TipoPedido.java para definir los tipos de pedidos en el JComboBox de la clase VentanaRegistro.

--- src/main/java/cl/duoc/vista/VentanaPrincipal.java
+++ src/main/java/cl/duoc/vista/VentanaPrincipal.java
@@ -1,4 +1,52 @@
 package cl.duoc.vista;
 
-public class VentanaPrincipal {
+
+import cl.duoc.model.ZonaDeCarga;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class VentanaPrincipal extends JFrame {
+
+    private JButton botonRegistrarPedido;
+    private JButton botonListaPedidos;
+    private JButton botonAsignarRepartidor;
+    private final ZonaDeCarga zonaDeCarga = new ZonaDeCarga();
+
+    public VentanaPrincipal() {
+        setTitle("Ventana Principal-SpeedFast");
+        setSize(400, 320);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        JPanel panelBotones = new JPanel(new GridLayout(3, 1, 0, 15));
+        panelBotones.setBorder(BorderFactory.createEmptyBorder(40, 50, 40, 50));
+
+        botonRegistrarPedido = new JButton("Registrar pedido");
+        botonListaPedidos = new JButton("Lista de pedidos");
+        botonAsignarRepartidor = new JButton("Asignar repartidor/Iniciar Reparto");
+
+        panelBotones.add(botonRegistrarPedido);
+        panelBotones.add(botonListaPedidos);
+        panelBotones.add(botonAsignarRepartidor);
+        setVisible(true);
+
+        add(panelBotones, BorderLayout.CENTER);
+
+        botonRegistrarPedido.addActionListener(e -> {
+            VentanaRegistroPedido registro = new VentanaRegistroPedido(zonaDeCarga);
+            registro.setVisible(true);
+        });
+
+        //botonListaPedidos.addActionListener(e -> {
+         //   VentanaListaPedidos listado = new VentanaListaPedidos(zonaDeCarga);
+        //    listado.setVisible(true);
+        //});
+
+
+    }
+
+
+
+
 }
--- src/main/java/cl/duoc/vista/VentanaRegistroPedido.java
+++ src/main/java/cl/duoc/vista/VentanaRegistroPedido.java
@@ -1,4 +1,55 @@
 package cl.duoc.vista;
 
-public class VentanaRegistroPedido {
+import cl.duoc.model.TipoPedido;
+import cl.duoc.model.ZonaDeCarga;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class VentanaRegistroPedido extends JFrame {
+    private ZonaDeCarga zonaDeCarga;
+    private JLabel lblId;
+    private JLabel lblDireccion;
+    private JLabel lblTipo;
+    private JTextField txtId;
+    private JTextField txtDireccion;
+    private JComboBox comboTipo;
+    private JButton btnGuardar;
+
+    public VentanaRegistroPedido(ZonaDeCarga zonaDeCarga) {
+        this.zonaDeCarga = zonaDeCarga;
+        this.setTitle("Registro de pedidos");
+        setSize(350, 250);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        JPanel panelRegistro = new JPanel(new GridLayout(4, 2, 10, 10));
+        panelRegistro.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
+
+
+        JLabel lblId = new JLabel("ID");
+        JLabel lblDireccion = new JLabel("Direccion");
+        JLabel lblTipo = new JLabel("Tipo");
+
+        txtId = new JTextField();
+        txtDireccion = new JTextField();
+        comboTipo = new JComboBox<>(TipoPedido.values());
+        btnGuardar = new JButton("Guardar");
+
+        panelRegistro.add(lblId);
+        panelRegistro.add(txtId);
+        panelRegistro.add(lblDireccion);
+        panelRegistro.add(txtDireccion);
+        panelRegistro.add(lblTipo);
+        panelRegistro.add(comboTipo);
+        panelRegistro.add(new JLabel(""));
+        panelRegistro.add(btnGuardar);
+
+        setContentPane(panelRegistro);
+
+        setVisible(true);
+
+    }
+
+
 }
